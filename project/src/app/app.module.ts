5c6233eeaa044c94189c9db2b7d91bdf294ad4c7
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-06-08 23:24:04 +0300

    Adding Navigation to the App

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
