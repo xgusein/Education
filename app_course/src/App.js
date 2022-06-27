e82f0fa8b5ab5f42c0586753a3377a5664f693b9
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-06-28 00:55:35 +0300

    A closer Look at Jsx

--- app_course/src/App.js
+++ app_course/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './Components/ExpenseItem';
@@ -25,6 +26,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement('div',{},React.createElement('h2', {},"Let\s start"),
+  //        React.createElement(Expenses, {items:expenses},)
+  // );
+
   return (
     <div className="App">
       <Expenses items={expenses} />
