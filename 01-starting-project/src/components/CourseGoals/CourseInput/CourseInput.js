cfb3faf4ac37b84bb2d00ee8206c6b58f33f6f87
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-07-04 22:59:34 +0300

    Styles

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => (props.invalid ? 'red' : 'black')};
-}
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.invalid ? 'red' : 'black')};
+//   }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-  background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+//   `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -52,13 +52,13 @@ const CourseInput = props => {
   };
 
   return (
-    <FormControl invalid={!isValid}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+    <form onSubmit={formSubmitHandler}>
+      <div invalid={styles['form-control']}>
         <labe>Course Goal</labe>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
-    </FormControl>
+    </form>
   );
 };
 
