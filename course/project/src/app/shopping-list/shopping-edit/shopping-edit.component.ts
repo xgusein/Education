80730ddb46bafc20defb721266d1f9e68b7de663
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-10-07 00:18:27 +0300

    correction

---
+++ course/project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { EventEmitter } from 'stream';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName,ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+
+}
