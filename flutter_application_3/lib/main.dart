42d9be02f5ea2f3e8bd003e2981bc3b528fac25d
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-10-15 23:19:10 +0300

    ToDo app

---
+++ flutter_application_3/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ToDo Application",
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  const Iskele({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping list application"),
+      ),
+      body: AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({super.key});
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  TextEditingController t1 = TextEditingController();
+  List alisverisListesi = [];
+
+  elemanEkle() {
+    setState(() {
+      alisverisListesi.add(t1.text);
+      t1.clear();
+    });
+  }
+
+  elemanCikar() {
+    setState(() {
+      alisverisListesi.remove(t1.text);
+      t1.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        Flexible(child: ListView.builder(
+          itemCount: alisverisListesi.length,
+          itemBuilder: (context, indeksNumarasi) => ListTile(
+            subtitle: Text("Alisveris Listesi"),
+            title: Text(alisverisListesi[indeksNumarasi]),),),),
+        TextField(
+          controller: t1,
+        ),
+        ElevatedButton(onPressed: elemanEkle, child: Text("Ekle")),
+        ElevatedButton(onPressed: elemanCikar, child: Text("Cikar")),
+      ]),
+    );
+  }
+}
