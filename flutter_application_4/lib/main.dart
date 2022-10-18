c20ee4b6854767f7c6914f43de4ef1a9a4e7fffd
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-10-19 00:41:10 +0300

    application 4

---
+++ flutter_application_4/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic chating application",
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  const Iskele({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mesajlasma Uygulamasi"),
+      ),
+      body: AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({super.key});
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  TextEditingController t1 = TextEditingController();
+  List mesajListesi = [];
+
+  mesajlariEkle(String metin) {
+    setState(() {
+      mesajListesi.insert(0, metin);
+      t1.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: mesajListesi.length,
+              itemBuilder: (context, indeksNumarasi) =>
+                  Text(mesajListesi[indeksNumarasi]),
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  onSubmitted: mesajlariEkle,
+                  controller: t1,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: mesajlariEkle(t1.text), child: Text("Send")),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
