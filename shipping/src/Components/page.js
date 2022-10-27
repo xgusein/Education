631bf2d6d3f6acfc455173a87947e94c2f19202d
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-10-28 00:59:39 +0300

    buttons

--- shipping/src/Components/page.js
+++ shipping/src/Components/page.js
@@ -1,3 +1,6 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {IoIosAlert} from 'react-icons/io';
 import './page.css'
 
 function Page() {
@@ -6,6 +9,17 @@ function Page() {
         <div className="box_header">
             <h1 className="heading">The Company</h1>
             <h2>Shipping Time <br /> Calculator</h2>
+            <div className="inputs">
+                <input type="date" />
+                <input type="text" required="required"/>
+                <span>Quantity<IoIosAlert className="Alert_icon"/></span>
+                <select id="Fabric" name="Fabric">
+                  <option value="">Fabric Type</option>
+                  <option value="saab">Cotton</option>
+                  <option value="fiat">Linen</option>
+                </select>
+            </div>
+            <button className="calculate">Calculate</button>
         </div>
       </div>
     );
